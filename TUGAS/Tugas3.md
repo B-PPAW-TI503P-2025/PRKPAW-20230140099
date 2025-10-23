3c8bf0e66db643bbb24738df537b370da6ef562b
Jaidil14 <jaidilrachmad@gmail.com>
2025-10-23 14:47:03 +0700

    Mengelola Routing dan Middleware di Express.js

---
+++ TUGAS/Tugas3.md
@@ -0,0 +1,16 @@
+# Tugas 3
+
+presensi/check-in:
+![Tampilan presensi_check-in](presensi_check-in.png)
+
+presensi/check-in jika dilakukan lebih dari 1 kali:
+![Tampilan presensi check-in jika dilakukan lebih dari 1 kali](presensi_check-in_lebih_dari_1x.png)
+
+presensi/check-out:
+![Tampilan presensi_check-out](presensi_check-out.png)
+
+presensi/check-out jika belum melakukan check-in:
+![Tampilan presensi check-out jika belum melakukan check-in](presensi_check-out_jika_belum_check-in.png)
+
+reports/daily:
+![Tampilan reports_daily](reports_daily.png)
--- my-node-server/server.js
+++ my-node-server/server.js
@@ -1,35 +1,43 @@
- 	const cors = require('cors');
-  const express = require('express');
- 	const bookRoutes = require('./routes/books');
- 	const app = express();
- 	const PORT = 3001;
- 	
- 	// Middleware
- 	app.use(cors()); 
- 	app.use(express.json()); 
- 	app.use((req, res, next) => {
- 	  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
- 	  next();
- 	});
- 	
- 	app.get('/', (req, res) => {
- 	  res.send('Home Page for API');
- 	});
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const PORT = 3001;
+const morgan = require("morgan");
+const bookRoutes = require("./routes/books");
 
-  app.use('/api/books', bookRoutes);
+// Import router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
 
-  app.use((req, res, next) => {
-    res.status(404).send("Error 404: Endpoint Tidak Ditemukan.");
-  });
- 	
-  app.use((err, req, res, next) => {
-      console.error(err.stack); // Logging error stack untuk debugging server
-      res.status(500).json({ 
-          message: "Terjadi Kesalahan Internal Server (500)",
-          error: err.message 
-      });
-  });
+// Middleware
+app.use(cors()); 
+app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+	console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+	next();
+});
 
- 	app.listen(PORT, () => {
- 	  console.log(`Express server running at http://localhost:${PORT}/`);
- 	});
+app.get('/', (req, res) => {
+	res.send('Home Page for API');
+});
+
+app.use('/api/books', bookRoutes);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
+
+app.use((req, res, next) => {
+res.status(404).send("Error 404: Endpoint Tidak Ditemukan.");
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.stack); // Logging error stack untuk debugging server
+	res.status(500).json({ 
+		message: "Terjadi Kesalahan Internal Server (500)",
+		error: err.message 
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Express server running at http://localhost:${PORT}/`);
+});
